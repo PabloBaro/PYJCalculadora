89e6baf36d364885489e0510cccb5b1eaf941fed
josemaxp <josemaxp@gmail.com>
2021-03-04 22:50:37 +0100

    Se han añadido los constructores, se ha arreglado varios bugs de texto y un bug que provocaba que no se saliese del programa si el número era dstinto a los mencionados en el programa.

--- PYJCalculadora.java
+++ PYJCalculadora.java
@@ -5,53 +5,65 @@ public class PYJCalculadora {
     int numero1;
     int numeroPorTeclado;
     
+    public PYJCalculadora(){
+        this.numero1 = 1;
+        this.numeroPorTeclado = 1;
+    }
+
+    public PYJCalculadora(int numero1, int numeroPorTeclado) {
+        this.numero1 = numero1;
+        this.numeroPorTeclado = numeroPorTeclado;
+    }
+    
     public static void main(String[] args) {
         Scanner teclado = new Scanner(System.in);
         int seleccion = 0;
+        Boolean salir = false;
         
         do{
             System.out.println("----------MENU----------");
             System.out.println("1. Suma");
             System.out.println("2. Resta");
-            System.out.println("3. División");
-            System.out.println("4. Multiplicación");
+            System.out.println("3. Division");
+            System.out.println("4. Multiplicacion");
             System.out.println("5. Salir");
-            System.out.print("¿Qué ecuación quiere elegir? ");
+            System.out.print("¿Que ecuacion quiere elegir? ");
             seleccion = teclado.nextInt();
             
             switch (seleccion) {
                 case 1:
-                    System.out.print("Introduzca un número a sumar: ");
+                    System.out.print("Introduzca un numero a sumar: ");
                     int sum1 = teclado.nextInt();
-                    System.out.print("Introduzca el segundo número a sumar: ");
+                    System.out.print("Introduzca el segundo numero a sumar: ");
                     int sum2 = teclado.nextInt();
                     System.out.println("La suma de " + sum1 + " + " + sum2 + " = "+"\u001B[36m"+(sum1+sum2)+"\u001B[0m");
                     break;
                 case 2:
-                    System.out.print("Introduzca un número a restar: ");
+                    System.out.print("Introduzca un numero a restar: ");
                     int resta1 = teclado.nextInt();
-                    System.out.print("Introduzca el segundo número a restar: ");
+                    System.out.print("Introduzca el segundo numero a restar: ");
                     int resta2 = teclado.nextInt();
                     System.out.println("La resta de " + resta1 + " - " + resta2 + " = "+"\u001B[36m"+(resta1-resta2)+"\u001B[0m");
                     break;
                 case 3:
-                    System.out.print("Introduzca un número para dividir: ");
+                    System.out.print("Introduzca un numero para dividir: ");
                     int div1 = teclado.nextInt();
-                    System.out.print("Introduzca el segundo para dividir: ");
+                    System.out.print("Introduzca el segundo numero para dividir: ");
                     int div2 = teclado.nextInt();
                     System.out.println("La division de " + div1 + " / " + div2 + " = "+"\u001B[36m"+(div1/div2)+"\u001B[0m");
                     break;
                 case 4:
-                    System.out.print("Introduzca un número a multiplicar: ");
+                    System.out.print("Introduzca un numero a multiplicar: ");
                     int multi1 = teclado.nextInt();
-                    System.out.print("Introduzca el segundo número a multiplicar: ");
+                    System.out.print("Introduzca el segundo numero a multiplicar: ");
                     int multi2 = teclado.nextInt();
-                    System.out.println("La multiplicación de " + multi1 + " * " + multi2 + " = "+"\u001B[36m"+(multi1*multi2)+"\u001B[0m");
+                    System.out.println("La multiplicacion de " + multi1 + " * " + multi2 + " = "+"\u001B[36m"+(multi1*multi2)+"\u001B[0m");
                     break;
                 default:
                     System.out.println("Saliendo del programa...");
+                    salir = true;
                     break;
             }
-        }while(seleccion!=5);
+        }while(salir == false);
     }
 }
